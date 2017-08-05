5bdd7d06a923e7cd5067e9e1ecf54a98676bcd62
FieryFeanor <neillock@yahoo.com>
2017-08-05 19:33:01 +1000

    Add project files.

---
+++ LockOnCode.VirtualMachine.Devices/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace LockOnCode.VirtualMachine.Devices
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
